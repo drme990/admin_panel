5f0572f4ca5863166a47771aeeffdd16f978ad6a
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2026-06-24 18:17:31 +0300

    refactor: allow default execution date selection from today instead of tomorrow in booking page
    - Changed minDate from tomorrow to egyptToday in DatePicker component
    - Enables selecting current day as default execution date when before cutoff time

--- app/(dashboard)/booking/page.tsx
+++ app/(dashboard)/booking/page.tsx
@@ -298,7 +298,7 @@ export default function BookingAdminPage() {
               value={defaultExecutionDate}
               onChange={setDefaultExecutionDate}
               markedDates={sortedDates}
-              minDate={tomorrow}
+              minDate={egyptToday}
               maxDate={dayAfterTomorrow}
               disabledDates={sortedDates}
             />
